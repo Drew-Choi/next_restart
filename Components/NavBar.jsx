825e20ee905faa37c52847bbfbe19d199759ad1c
Drew <drc920630@gmail.com>
2023-07-14 22:59:13 +0900

    넥스트 버젼 상태 체크

--- Components/NavBar.jsx
+++ Components/NavBar.jsx
@@ -1,13 +1,19 @@
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 
-const NavBar = () => {
-  const router = useRouter();
+export async function getServerSideProps() {
+  console.log('server');
 
+  return {
+    props: { time: new Date().toISOString() },
+  };
+}
+
+const NavBar = ({ time }) => {
   return (
     <nav>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
+      <span style={{ color: 'black' }}>시간: {time}</span>
     </nav>
   );
 };
